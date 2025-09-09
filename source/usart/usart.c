12d07fadfb7d955221f56412df698b72915fd8fa
Marius Bär <marius.baer@proton.me>
2025-09-09 15:00:13 +0000

    Rewrite the usart init, tx_ready and transmit functions

--- source/usart/usart.c
+++ source/usart/usart.c
@@ -8,40 +8,81 @@
 #include "usart.h"
 
 #include <avr/io.h>
-#include <stdbool.h>
+
+#define USART0 ((usart_t *)(&UBRR0L))
+#define USART1 ((usart_t *)(&UBRR1L))
+
+/* UCSRA bit definitions */
+#define RXC  RXC0
+#define TXC  TXC0
+#define UDRE UDRE0
+#define FE   FE0
+#define DOR  DOR0
+#define UPE  UPE0
+#define U2X  U2X0
+#define MPCM MPCM0
+
+/* UCSRB bit definitions */
+#define RXCIE RXCIE0
+#define TXCIE TXCIE0
+#define UDRIE UDRIE0
+#define RXEN  RXEN0
+#define TXEN  TXEN0
+#define UCSZ2 UCSZ02
+#define RXB8  RXB80
+#define TXB8  TXB80
+
+/* UCSRC bit definitions */
+#define URSEL URSEL0
+#define UMSEL UMSEL0
+#define UPM1  UPM01
+#define UPM0  UPM00
+#define USBS  USBS0
+#define UCSZ1 UCSZ01
+#define UCSZ0 UCSZ00
+#define UCPOL UCPOL0
 
 /**
  * @brief Base function to initialize USART0 or USART1 in asynchronous mode with 8 data bits and 2
  * stop bits.
+ * @param usart Pointer to the usart struct.
+ * @param ubrr The value to be set in the UBRR register to configure the baud rate.
+ * @note The baud rate can be calculated using the formula (see datasheet page 169):
+ * \f$ UBRR = \frac{F_{\mathrm{OSC}}}{16 \cdot \mathrm{Baud}} - 1 \f$
  */
-static inline void usart_init(uint16_t ubrr, volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
-                              volatile uint8_t *ucsrb, volatile uint8_t *ucsrc, uint8_t rxen,
-                              uint8_t txen, uint8_t usbs, uint8_t ucsz0) {
+void usart_init(volatile usart_t *usart, uint16_t ubrr) {
     /* Set baud rate */
-    *ubrrh = (uint8_t)(ubrr >> 8);
-    *ubrrl = (uint8_t)ubrr;
+    volatile uint8_t *ubrrh_c;
+
+    if (usart == USART0) {
+        ubrrh_c = &UBRR0H;
+    } else {
+        ubrrh_c = &UBRR1H;
+    }
+    *ubrrh_c = (uint8_t)(ubrr >> 8);
+    usart->ubrrl = (uint8_t)ubrr;
     /* Enable receiver and transmitter */
-    *ucsrb = (1 << rxen) | (1 << txen);
+    usart->ucsrb = (1 << RXEN) | (1 << TXEN);
     /* Set frame format: 8data, 2stop bit */
-    *ucsrc = (1 << usbs) | (3 << ucsz0);
+    *ubrrh_c = (1 << USBS) | (3 << UCSZ0);
 }
 
 /**
- * @brief Initialize USART0 in asynchronous mode with 8 data bits and 2 stop bits.
- * @param ubrr The value to be set in the UBRR register to configure the baud rate.
- * @note The baud rate can be calculated using the formula (see datasheet page 169):
- * \f$ UBRR = \frac{F_{\mathrm{OSC}}}{16 \cdot \mathrm{Baud}} - 1 \f$
+ * @brief Check if the USART transmit buffer is empty.
+ * @param usart Pointer to the usart struct.
  */
-void usart0_init(uint16_t ubrr) {
-    usart_init(ubrr, &UBRR0H, &UBRR0L, &UCSR0B, &UCSR0C, RXEN0, TXEN0, USBS0, UCSZ00);
+bool usart_tx_ready(volatile usart_t *usart) {
+    return (usart->ucsra & (1 << UDRE));
 }
 
 /**
- * @brief Initialize USART1 in asynchronous mode with 8 data bits and 2 stop bits.
- * @param ubrr The value to be set in the UBRR register to configure the baud rate.
- * @note The baud rate can be calculated using the formula (see datasheet page 169):
- * \f$ UBRR = \frac{F_{\mathrm{OSC}}}{16 \cdot \mathrm{Baud}} - 1 \f$
+ * @brief Transmit a single byte of data via USART.
+ * @param usart Pointer to the usart struct.
+ * @param data The byte of data to be transmitted.
  */
-void usart1_init(uint16_t ubrr) {
-    usart_init(ubrr, &UBRR1H, &UBRR1L, &UCSR1B, &UCSR1C, RXEN1, TXEN1, USBS1, UCSZ10);
+void usart_transmit(volatile usart_t *usart, uint8_t data) {
+    /* Wait for empty transmit buffer */
+    while (!usart_tx_ready(usart));
+    /* Put data into buffer, sends the data */
+    usart->udr = data;
 }
--- source/usart/usart.h
+++ source/usart/usart.h
@@ -8,9 +8,18 @@
 #ifndef USART_H
 #define USART_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
-void usart0_init(uint16_t ubrr);
-void usart1_init(uint16_t ubrr);
+typedef struct {
+    volatile uint8_t ubrrl;
+    volatile uint8_t ucsrb;
+    volatile uint8_t ucsra;
+    volatile uint8_t udr;
+} usart_t;
+
+void usart_init(volatile usart_t *usart, uint16_t ubrr);
+bool usart_tx_ready(volatile usart_t *usart);
+void usart_transmit(volatile usart_t *usart, uint8_t data);
 
 #endif /* USART_H */
