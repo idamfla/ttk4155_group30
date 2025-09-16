0a76e262a981b52e0575f93cd5812d86a4e4841e
Marius Bär <marius.baer@proton.me>
2025-09-16 16:01:12 +0000

    Enable the external memory interface and configure the high mask

--- source/main.c
+++ source/main.c
@@ -8,6 +8,13 @@
 
 int main(void) {
     printf_init(USART0, UBRR0);
+
+    // Configure external memory high mask to only use four lines of the high byte of the address.
+    // This release the pins PC4 to PC7 (for JTAG).
+    SFIOR = (1 << XMM2);
+    // Enable external SRAM.
+    MCUCR = (1 << SRE);
+
     while (1) {
         uint8_t *p;
         p = (uint8_t *)0x1FFF;
