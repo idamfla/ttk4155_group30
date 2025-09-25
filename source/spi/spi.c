fd80c37c6c355a35acdd07555ed1d5181d30ca15
idamfla <idamfla@stud.ntnu.no>
2025-09-25 11:54:15 +0200

    written 'write to slave' and 'read from slave'

--- source/spi/spi.c
+++ source/spi/spi.c
@@ -11,8 +11,13 @@
 void SPI_MasterInit(void) {
     /* Set MOSI and SCK output, all others input */
     DDR_SPI = (1 << DD_MOSI) | (1 << DD_SCK);
+    // DDR_SPI &= ~(1 << PB6);  // MISO as input
     /* Enable SPI, Master, set clock rate fck/16 */
     SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+
+    /* Set slave pins as output, and set them high (SS\bar) */
+    DDR_SLAVES = (1 << SS1) | (1 << SS2) | (1 << SS2);
+    PORT_SLAVES |= (1 << SS1) | (1 << SS2) | (1 << SS2);
 }
 void SPI_MasterTransmit(char cData) {
     /* Start transmission */
@@ -21,15 +26,48 @@ void SPI_MasterTransmit(char cData) {
     while (!(SPSR & (1 << SPIF)));
 }
 
-void SPI_SlaveInit(void) {
-    /* Set MISO output, all others input */
-    DDR_SPI = (1 << DD_MISO);
-    /* Enable SPI */
-    SPCR = (1 << SPE);
+
+
+/* Writing to slaves */
+void SPI_write(uint8_t data) {
+    SPI_write_to_slave(SS1, data);
 }
-char SPI_SlaveReceive(void) {
-    /* Wait for reception complete */
-    while (!(SPSR & (1 << SPIF)));
-    /* Return data register */
-    return SPDR;
+
+void SPI_write_to_slave(uint8_t slaveSS, uint8_t data) {
+    PORT_SLAVES &= ~(1 << slaveSS);
+
+    SPDR = data;
+    while (!(SPDR & (1 << SPIF)));
+
+    PORT_SLAVES |= (1 << slaveSS);
 }
+
+
+
+/* Reading from slaves */
+uint8_t SPI_read() {
+    return SPI_read_from_slave(SS1);
+}
+uint8_t SPI_read_from_slave(uint8_t slaveSS) {
+    PORT_SLAVES &= ~(1 << slaveSS);
+    SPDR = BUFFER_DATA;
+    while (!(SPDR & (1 << SPIF)));
+
+    uint8_t recieved = SPDR;
+    PORT_SLAVES |= (1 << slaveSS);
+
+    return recieved;
+}
+
+// void SPI_SlaveInit(void) {
+//     /* Set MISO output, all others input */
+//     DDR_SPI = (1 << DD_MISO);
+//     /* Enable SPI */
+//     SPCR = (1 << SPE);
+// }
+// char SPI_SlaveReceive(void) {
+//     /* Wait for reception complete */
+//     while (!(SPSR & (1 << SPIF)));
+//     /* Return data register */
+//     return SPDR;
+// }
--- source/spi/spi.h
+++ source/spi/spi.h
@@ -9,11 +9,25 @@
 #pragma once;
 #include <avr/io.h>
 
-#define MISO PB5;
-#define MOSI ;
+#define DDR_SPI DDRB
+#define DD_MOSI DDB5
+#define DD_SCK  DDB7
+// #define SS DDB4
+#define DDR_SLAVES  DDRA
+#define PORT_SLAVES PORTA
+#define SS1         DDA0
+#define SS2         DDA1
+#define SS3         DDA2
+#define BUFFER_DATA 0x00
 
 void SPI_MasterInit(void);
 void SPI_MasterTransmit(char cData);
 
-void SPI_SlaveInit(void);
-char SPI_SlaveReceive(void);
+void SPI_write(uint8_t data);
+void SPI_write_to_slave(uint8_t slaveSS, uint8_t data);
+
+uint8_t SPI_read();
+uint8_t SPI_read_from_slave(uint8_t slaveSS);
+
+// void SPI_SlaveInit(void);
+// char SPI_SlaveReceive(void);
