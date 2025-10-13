6d56d475ebc88d038d407c2a927b869350d29c62
Marius Bär <marius.baer@proton.me>
2025-10-13 11:48:21 +0000

    Tests to increase the response time of the SPI interrupt handling

--- source/spi/spi.c
+++ source/spi/spi.c
@@ -56,16 +56,17 @@ static spi_queue_t _spi_queue = {.max_size = QUEUE_SIZE,
 static volatile uint16_t _data_idx;
 static spi_transfer_t _transfer;
 static volatile bool _transfer_active = false;
+static volatile uint8_t tx_data;
 
 static void _spi_slave_select(uint8_t slave_idx);
 static void _spi_slave_deselect(uint8_t slave_idx);
 
 static inline void _spi_rxtx(void) {
-    if (_transfer.tx_data) {
-        SPDR = _transfer.tx_data[_data_idx];
-    } else {
-        SPDR = 0;
-    }
+    // if (_transfer.tx_data) {
+    //     SPDR = _transfer.tx_data[_data_idx];
+    // } else {
+    //     SPDR = 0;
+    // }
 }
 
 static void _spi_slave_select(uint8_t slave_idx) {
@@ -109,8 +110,16 @@ void _spi_next_transfer(void) {
     _transfer_active = true;
     _data_idx = 0;
     _transfer = spi_queue_pop(&_spi_queue);
+    if (_transfer.tx_data) {
+        tx_data = _transfer.tx_data[_data_idx];
+    } else {
+        tx_data = 0;
+    }
     _spi_slave_select(_transfer.slave_idx);
-    _spi_rxtx();
+    SPDR = tx_data;
+    ++_data_idx;
+    tx_data = (_transfer.tx_data) ? _transfer.tx_data[_data_idx] : 0;
+    // _spi_rxtx();
 }
 
 void spi_master_init(void) {
@@ -143,13 +152,25 @@ bool spi_transfer(const spi_transfer_t* transfer) {
 }
 
 ISR(SPI_STC_vect) {
-    if (_transfer.rx_data) {
-        _transfer.rx_data[_data_idx] = SPDR;
-    }
-    ++_data_idx;
+    static uint8_t rx_data;
+    rx_data = SPDR;
+    // SPDR = tx_data;
+
+    //  if (_transfer.rx_data) {
+    //      _transfer.rx_data[_data_idx] = SPDR;
+    //  }
     if (_data_idx < _transfer.length) {
-        _spi_rxtx();
+        //_spi_rxtx();
+        SPDR = tx_data;
+        if (_transfer.rx_data) {
+            _transfer.rx_data[_data_idx - 1] = rx_data;
+        }
+        ++_data_idx;
+        tx_data = (_transfer.tx_data) ? _transfer.tx_data[_data_idx] : 0;
     } else {
+        if (_transfer.rx_data) {
+            _transfer.rx_data[_data_idx - 1] = rx_data;
+        }
         _spi_slave_deselect(_transfer.slave_idx);
         _transfer_active = false;
         if (_transfer.transfer_cmplt_cbk) {
