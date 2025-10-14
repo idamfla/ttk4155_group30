1ade2c07d3f7aab873e72228e90ce37e4df198ea
Marius Bär <marius.baer@proton.me>
2025-10-14 15:16:25 +0000

    Write some demo menu system with a submenu

--- source/ui/ui.c
+++ source/ui/ui.c
@@ -21,21 +21,48 @@ static uint8_t _graphics_buffer[UI_BUFFER_SIZE];
 static ui_element_t* _ui_element_stack[ELEMENT_STACK_SIZE];
 static ui_event_t _ui_event_buffer[EVENT_QUEUE_SIZE];
 static ui_menu_static_t _main_menu;
+static ui_menu_static_t _sub_menu;
 
 static void main_menu_init(void);
 static ui_event_status_t main_menu_on_event(ui_menu_static_t* const me, const ui_event_t event);
+static ui_event_status_t sub_menu_on_event(ui_menu_static_t* const me, const ui_event_t event);
 
 static void main_menu_init(void) {
     static const char* menu_items[] = {
-        "Item 1",
-        "Item 2",
-        "Item 3",
+        "Item 1", "Item 2", "Item 3", "Submenu", "Item 5",
+        "Item 6", "Item 7", "Item 8", "Item 9",  "Item 10",
     };
     ui_menu_static_ctor(&_main_menu, main_menu_on_event, menu_items,
                         sizeof(menu_items) / sizeof(menu_items[0]));
 }
 
+static void sub_menu_init(void) {
+    static const char* menu_items[] = {"Sub 1", "Sub 2", "Sub 3"};
+    ui_menu_static_ctor(&_sub_menu, sub_menu_on_event, menu_items,
+                        sizeof(menu_items) / sizeof(menu_items[0]));
+}
+
 static ui_event_status_t main_menu_on_event(ui_menu_static_t* const me, const ui_event_t event) {
+    ui_event_status_t status;
+    (void)me; /* unused parameter */
+    switch (event) {
+        // case ui_event_button_select:
+        //     ui_element_push(&ui, (ui_element_t*)&_sub_menu);
+        //     status = ui_event_status_handled;
+        //     break;
+        case ui_event_button_select:
+            if (me->current_item == 3U) {
+                ui_element_push(&ui, (ui_element_t*)&_sub_menu);
+                status = ui_event_status_handled;
+            }
+        default:
+            status = ui_event_status_ignored;
+            break;
+    }
+    return status;
+}
+
+static ui_event_status_t sub_menu_on_event(ui_menu_static_t* const me, const ui_event_t event) {
     ui_event_status_t status;
     (void)me; /* unused parameter */
     switch (event) {
@@ -54,6 +81,7 @@ void ui_init(void) {
     ui_ctor(&ui, _ui_element_stack, ELEMENT_STACK_SIZE, _ui_event_buffer, EVENT_QUEUE_SIZE,
             _graphics_buffer);
     main_menu_init();
+    sub_menu_init();
 
     ui_element_push(&ui, (ui_element_t*)&_main_menu);
 }
