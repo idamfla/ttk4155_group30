bcc0223eb71e3859d82c3c490024f948ca1d90f8
Marius Bär <marius.baer@proton.me>
2025-10-21 15:42:09 +0000

    Formatting

--- source/io_board/io_board.c
+++ source/io_board/io_board.c
@@ -34,14 +34,14 @@ typedef enum {
 
 typedef struct {
     union {
-        void (*get_cmplt_cbk)(volatile void *data);
+        void (*get_cmplt_cbk)(volatile void* data);
         void (*set_cmplt_cbk)(void);
     };
     bool get;
 } io_transfer_param_t;
 
 void io_spi_start_cbk(void);
-void io_transfer_cmplt(void *unused);
+void io_transfer_cmplt(void* unused);
 
 volatile uint8_t _rx_data[35];
 volatile io_command_t _cmd;
@@ -60,7 +60,7 @@ void io_spi_start_cbk(void) {
     _delay_us(24);
 }
 
-void io_transfer_cmplt(void *unused) {
+void io_transfer_cmplt(void* unused) {
     (void)unused;  // unused
     if (_transfer_cmplt_param.get) {
         _transfer_cmplt_param.get_cmplt_cbk(_transfer_data.rx_data);
@@ -90,7 +90,7 @@ bool io_get_touch_pad(io_get_touch_pad_cmplt_t cmplt_cbk) {
     _transfer_data.length = sizeof(io_touch_pad_t);
 
     _transfer_cmplt_param.get = true;
-    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void *))cmplt_cbk;
+    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void*))cmplt_cbk;
 
     return spi_transfer(&_transfer_data);
 }
@@ -114,7 +114,7 @@ bool io_get_touch_slider(io_get_touch_slider_cmplt_t cmplt_cbk) {
     _transfer_data.length = sizeof(io_touch_slider_t);
 
     _transfer_cmplt_param.get = true;
-    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void *))cmplt_cbk;
+    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void*))cmplt_cbk;
 
     return spi_transfer(&_transfer_data);
 }
@@ -137,7 +137,7 @@ bool io_get_joystick(io_get_joystick_cmplt_t cmplt_cbk) {
     _transfer_data.length = sizeof(io_joystick_t);
 
     _transfer_cmplt_param.get = true;
-    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void *))cmplt_cbk;
+    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void*))cmplt_cbk;
 
     return spi_transfer(&_transfer_data);
 }
@@ -160,7 +160,7 @@ bool io_get_buttons(io_get_buttons_cmplt_t cmplt_cbk) {
     _transfer_data.length = sizeof(io_buttons_t);
 
     _transfer_cmplt_param.get = true;
-    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void *))cmplt_cbk;
+    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void*))cmplt_cbk;
 
     return spi_transfer(&_transfer_data);
 }
@@ -183,7 +183,7 @@ bool io_get_info(io_get_info_cmplt_t cmplt_cbk) {
     _transfer_data.length = sizeof(io_info_t);
 
     _transfer_cmplt_param.get = true;
-    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void *))cmplt_cbk;
+    _transfer_cmplt_param.get_cmplt_cbk = (void (*)(volatile void*))cmplt_cbk;
 
     return spi_transfer(&_transfer_data);
 }
@@ -195,14 +195,14 @@ bool io_get_info(io_get_info_cmplt_t cmplt_cbk) {
  * @retval true if the command was sent
  * @retval false if a transfer was already active and the new command could not be sent
  */
-bool io_set_led_on_off(io_led_on_off_t *led_state, io_set_cmplt_t cmplt_cbk) {
+bool io_set_led_on_off(io_led_on_off_t* led_state, io_set_cmplt_t cmplt_cbk) {
     if (_transfer_active) {
         return false;
     }
     _transfer_active = true;
     _cmd = io_cmd_set_led_on_off;
 
-    _transfer_data.tx_data = (uint8_t *)led_state;
+    _transfer_data.tx_data = (uint8_t*)led_state;
     _transfer_data.rx_data = NULL;
     _transfer_data.length = sizeof(io_led_on_off_t);
 
@@ -219,14 +219,14 @@ bool io_set_led_on_off(io_led_on_off_t *led_state, io_set_cmplt_t cmplt_cbk) {
  * @retval true if the command was sent
  * @retval false if a transfer was already active and the new command could not be sent
  */
-bool io_set_led_pwm(io_led_pwm_t *led_state, io_set_cmplt_t cmplt_cbk) {
+bool io_set_led_pwm(io_led_pwm_t* led_state, io_set_cmplt_t cmplt_cbk) {
     if (_transfer_active) {
         return false;
     }
     _transfer_active = true;
     _cmd = io_cmd_set_led_pwm;
 
-    _transfer_data.tx_data = (uint8_t *)led_state;
+    _transfer_data.tx_data = (uint8_t*)led_state;
     _transfer_data.rx_data = NULL;
     _transfer_data.length = sizeof(io_led_pwm_t);
 
