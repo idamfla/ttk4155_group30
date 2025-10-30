7e13053e8eaefbb21060acbec7e6430b113aded4
Marius Bär <marius.baer@proton.me>
2025-10-30 14:59:44 +0000

    Configure CAN for a sample point at 87.5% and a Bitrate of 250 kbit/s and send dummy data

--- node_2/constants.h
+++ node_2/constants.h
@@ -4,4 +4,15 @@
 #define F_CPU     84U * 1000U * 1000U  // CPU frequency in Hz
 #define UART_BAUD 38400U               // UART baud rate
 
+#define F_CAN          (250U * 1000U)
+#define CAN_SJW        2U
+#define CAN_PROP_SEG   8U
+#define CAN_PHASE_SEG1 5U
+#define CAN_PHASE_SEG2 2U
+#define CAN_TQ         (1U /* Sync Seg*/ + CAN_PROP_SEG + CAN_PHASE_SEG1 + CAN_PHASE_SEG2)
+#define CAN_BRP        (F_CPU / F_CAN / CAN_TQ - 1U)
+
+_Static_assert(CAN_TQ == 16, "CAN_TQ must be 16");
+_Static_assert(CAN_BRP > 0, "CAN_BRP cannot be 0");
+
 #endif  // CONSTANTS_H
--- node_2/main.c
+++ node_2/main.c
@@ -1,9 +1,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include "can/can_controller.h"
 #include "constants.h"
 #include "sam.h"
 #include "uart/uart.h"
+
+CAN_MESSAGE msg = {
+    .id = 0x1,
+    .data_length = 4U,
+    .data = {0, 1, 2, 3},
+};
 
 int main() {
     SystemInit();
@@ -13,6 +20,18 @@ int main() {
 
     uart_init(F_CPU, UART_BAUD);
     printf("Hello World\r\n");
+
+    can_br_t can_br = {
+        .sjw = (CAN_SJW - 1U),
+        .prop_seg = (CAN_PROP_SEG - 1U),
+        .phase_seg1 = (CAN_PHASE_SEG1 - 1U),
+        .phase_seg2 = (CAN_PHASE_SEG2 - 1U),
+        .brp = (F_CPU / F_CAN / CAN_TQ),
+        .smp = CAN_SMP_ONCE,
+    };
+    can_init_def_tx_rx_mb(can_br.value);
+
+    can_send(&msg, 0U);
 
     while (1) {
         /* code */
